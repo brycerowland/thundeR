41470473fb7d74ce654f0ea4e678ee9003c793b1
Bryce Rowland <bryce.rowland@unc.edu>
2020-09-29 12:38:00 -0400

    mutate basis matrix functionality added with some tests

--- R/mutate_basis_matrix.R
+++ R/mutate_basis_matrix.R
@@ -1,9 +1,28 @@
-mutate_basis_matrix <- function(nmf_obj, type, name){
+#' Compute summary statistics on rows of cell type profile matrix
+#'
+#' @importFrom NMF basis
+#' @importFrom stringr str_c
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr rowwise
+#' @importFrom dplyr mutate
+#' @importFrom dplyr ungroup
+#' @importFrom NMF featureScore
+#' @importFrom tibble rownames_to_column
+#'
+#' @param nmf_obj NMF object - usually the output of `initial_nmf_fit`
+#'
+#'
+#' @export
+mutate_basis_matrix <- function(nmf_obj){
   .basis <- basis(nmf_obj)
 
   colnames(.basis) <- str_c("celltype", seq(1:dim(.basis)[[2]]), "_features")
 
-  .basis %>%
-    as_tibble() %>%
-    mutate(featureScore(nmf_obj))
+  .basis  %>%
+    as_tibble(rownames = "bin_pair") %>%
+    rowwise() %>%
+    mutate(std_dev = sd(c_across(starts_with("celltype")))) %>%
+    ungroup() %>%
+    mutate(feature_score = featureScore(nmf_obj)) %>%
+    return()
 }
--- tests/testthat/test-mutate_basis_matrix.R
+++ tests/testthat/test-mutate_basis_matrix.R
@@ -1,20 +1,52 @@
 test_that("colnames are formatted correctly", {
 
   #Generate some random number of cell types
-  k <- sample(1:100, size = 1)
+  k <- sample(2:10, 1)
   test_fit <- thunder("example_data/test_data.txt.gz", n_cell_types = k,
           itter = 1)
 
   #We want to check that the first k columns are formatted as we expect.
   check_cols <- mutate_basis_matrix(test_fit) %>%
-    select(all_of(1:k))
+    select(all_of(2:(k+1))) %>%
+    colnames()
 
 
-  any(str_detect(check_cols, "celltype[0-9]*_features"))
+  expect_true(all(str_detect(check_cols, "celltype[0-9]*_features")))
 })
 
 test_that("std deviation works with more than two cell types", {
 
+  test_fit <- thunder("example_data/test_data.txt.gz", n_cell_types = 5,
+                      itter = 1)
 
+  sd_vec <- mutate_basis_matrix(test_fit) %>%
+    pull(std_dev)
+
+  expect_true(is.numeric(sd_vec))
+  expect_true(all(sd_vec > 0))
+
+})
+
+test_that("feature score works with more than two cell types", {
+
+  test_fit <- thunder("example_data/test_data.txt.gz", n_cell_types = 5,
+                      itter = 1)
+
+  fs_vec <- mutate_basis_matrix(test_fit) %>%
+    pull(feature_score)
+
+  expect_true(is.numeric(fs_vec))
+
+})
+
+test_that("bin_pair column exists", {
+
+  test_fit <- thunder("example_data/test_data.txt.gz", n_cell_types = 5,
+                      itter = 1)
+
+  bp_vec <- mutate_basis_matrix(test_fit) %>%
+    pull(bin_pair)
+
+  expect_true(is.character(bp_vec))
 
 })
