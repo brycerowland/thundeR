09b366a7f2d21fb4ffd057d0f4f47ca320dff28d
Bryce Rowland <bryce.rowland@unc.edu>
2020-09-29 16:45:21 -0400

    fix tests.

--- tests/testthat/test-thunder.R
+++ tests/testthat/test-thunder.R
@@ -1,10 +1,10 @@
 test_that("out_init_nmf option outputs a .rds file", {
 
-  thunder("example_data/test_data.txt.gz", n_cell_types = 2,
+  thunder("test_data.txt.gz", n_cell_types = 2,
           itter = 1,
-          out_init_nmf = "example_data/test_data_init_out.rds")
+          out_init_nmf = "test_data_init_out.rds")
 
-  expect_true(file.exists("example_data/test_data_init_out.rds"))
-  file.remove("example_data/test_data_init_out.rds")
+  expect_true(file.exists("test_data_init_out.rds"))
+  file.remove("test_data_init_out.rds")
 })
 
