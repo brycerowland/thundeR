6984404b8051110c2d60335cd4e9e81ed65fcf9d
Bryce Rowland <bryce.rowland@unc.edu>
2021-05-18 13:18:06 -0400

    Fix bug in feature selection code.

--- R/mutate_basis_matrix.R
+++ R/mutate_basis_matrix.R
@@ -44,6 +44,7 @@ mutate_basis_matrix <- function(nmf_obj){
 #' @importFrom stringr str_split
 #' @importFrom tidyr unnest
 #'
+#'
 #' @param mixture_data mixture data from Hi-C experiment.
 #' @param nmf_obj NMF object - usually the output of `initial_nmf_fit`
 #'
@@ -52,12 +53,19 @@ mutate_basis_matrix <- function(nmf_obj){
 subset_init_nmf <- function(mixture_data, nmf_obj){
   .basis_m <- mutate_basis_matrix(nmf_obj)
 
-  rows <- .basis_m %>%
-    group_by(contact_type) %>%
-    summarise(thunder_bins = list(which(
-      (.$feature_score > mean(.$feature_score) + 3*sd(.$feature_score)) |
-        (.$std_dev > mean(.$std_dev) + 3*sd(.$std_dev))
-    ))) %>%
+  rows <- bind_rows(
+    .basis_m %>%
+      mutate(index = row_number()) %>%
+      filter(contact_type ==  "inter") %>%
+      filter(feature_score > mean(feature_score) + 3*sd(feature_score) |
+               std_dev > mean(std_dev) + 3 * sd(std_dev)) %>%
+      summarize(thunder_bins = list(index)),
+    .basis_m %>%
+      mutate(index = row_number()) %>%
+      filter(contact_type ==  "intra") %>%
+      filter(feature_score > median(feature_score) + 3*mad(feature_score)) %>%
+      summarize(thunder_bins = list(index))
+  ) %>%
     tidyr::unnest(thunder_bins) %>%
     ungroup() %>%
     arrange(thunder_bins) %>%
